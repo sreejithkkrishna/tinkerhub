d25bfc1eb2cb0e02c10ca8df22560b236e3d5d8a
Your Name <you@example.com>
2021-05-26 23:19:51 +0530

    first commit

--- project/config.py
+++ project/config.py
@@ -1,9 +1,8 @@
 import os
-cwd = os.path.abspath(os.path.dirname(__file__))
-db_path = 'sqlite:///'+os.path.join(cwd, 'db.sqlite')
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = db_path
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
